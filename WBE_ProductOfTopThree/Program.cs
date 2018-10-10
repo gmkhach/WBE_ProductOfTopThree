3b6037d0dbc876e39e28e64acec670796d6e33b3
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-10 08:43:40 -0400

    brute-force method

--- WBE_ProductOfTopThree/Program.cs
+++ WBE_ProductOfTopThree/Program.cs
@@ -10,6 +10,48 @@ namespace WBE_ProductOfTopThree
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("\nInput an array of integers\n\n>>> ");
+                    int[] input = Console.ReadLine().Split(',').Select(x => int.Parse(x)).ToArray();
+                    Console.WriteLine($"\nProduct of top 3 numbers: {ProductOfTopThree(input)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message);
+                }
+                Console.Write("\nPress Enter to do another division...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        private static int ProductOfTopThree(int[] arr)
+        {
+            int max1 = 0;
+            int max2 = 0;
+            int max3 = 0;
+            foreach (var n in arr)
+            {
+                if (n > max1)
+                {
+                    max3 = max2;
+                    max2 = max1;
+                    max1 = n;
+                }
+                else if (n > max2)
+                {
+                    max3 = max2;
+                    max2 = n;
+                }
+                else if (n > max3)
+                {
+                    max3 = n;
+                }
+            }
+            return max1 * max2 * max3;
         }
     }
 }
